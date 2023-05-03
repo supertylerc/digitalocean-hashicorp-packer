c638946017b72dfb261a213a6568a399a64676cc
Tyler Christiansen <code@tylerc.me>
2023-05-02 17:16:47 -0700

    feat: TailScale

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,4 +1,6 @@
 dnf update -y
 dnf install -y dnf-plugins-core
-dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
-dnf -y install consul
+dnf config-manager -y --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+dnf config-manager -y --add-repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf -y install consul tailscale
+sudo systemctl enable --now tailscaled
